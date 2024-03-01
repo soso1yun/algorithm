d1686d0b96694aa15350b7b86ad43e5a5930377f
soso1yun-at-522339398443 <zRKAKNnlV/Wv5HpoLWLVokbcebhW2ZErcyhCMzygReU=>
2024-03-01 14:40:30 +0900

    Hello World!를 출력하시오.

---
+++ src/main/java/com/example/algorithm/baekjoon/step1/HelloWorld2557.java
@@ -0,0 +1,13 @@
+package com.example.algorithm.baekjoon.step1;
+
+
+/**
+ * Hello World!를 출력하시오.
+ */
+public class HelloWorld2557 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
