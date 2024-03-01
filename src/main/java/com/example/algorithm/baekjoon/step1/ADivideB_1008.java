cd8a136027d25fa23abde45d0f919ee985dff6e5
soso1yun-at-522339398443 <zRKAKNnlV/Wv5HpoLWLVokbcebhW2ZErcyhCMzygReU=>
2024-03-01 15:01:07 +0900

    사칙연산 까지

---
+++ src/main/java/com/example/algorithm/baekjoon/step1/ADivideB_1008.java
@@ -0,0 +1,17 @@
+package com.example.algorithm.baekjoon.step1;
+
+
+import java.util.Scanner;
+
+/**
+ * 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+ */
+public class ADivideB_1008 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.nextDouble() / scanner.nextDouble());
+    }
+
+}
--- src/main/java/com/example/algorithm/baekjoon/step1/HelloWorld_2557.java
+++ src/main/java/com/example/algorithm/baekjoon/step1/HelloWorld_2557.java
@@ -4,7 +4,7 @@ package com.example.algorithm.baekjoon.step1;
 /**
  * Hello World!를 출력하시오.
  */
-public class HelloWorld2557 {
+public class HelloWorld_2557 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
