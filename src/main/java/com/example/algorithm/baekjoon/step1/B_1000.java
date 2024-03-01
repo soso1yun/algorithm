e41f67f90ee0c993f0ca5fd41b4d11d1b28eff75
soso1yun-at-522339398443 <zRKAKNnlV/Wv5HpoLWLVokbcebhW2ZErcyhCMzygReU=>
2024-03-01 15:02:52 +0900

    이름짓기가 힘듬..

--- src/main/java/com/example/algorithm/baekjoon/step1/B_1000.java
+++ src/main/java/com/example/algorithm/baekjoon/step1/B_1000.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /**
  * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
  */
-public class APlusB_1000 {
+public class B_1000 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
